ff4518ae3fd407e4d00c4e671b02cc61ce4c1683
konveloper <konveloper@gmail.com>
2023-03-28 17:26:15 +0900

    docs : 로그인 페이지 제작

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoImg from 'assets/img/Logo-hodu.png';
+import Button from 'components/Common/Button/Button';
+import Input from 'components/Common/Input/Input';
+import { ContSection, H2IR, LogoImg, InputForm } from './LoginStyle';
+
 export default function Login() {
   return (
-    <>
-      <p>로그인</p>
-    </>
+    <ContSection>
+      <H2IR>로그인 페이지</H2IR>
+      <LogoImg src={logoImg} alt='로고 이미지' />
+      <InputForm>
+        <Input name='email' label='이메일' type='email' />
+        <Input name='pw' label='비밀번호' type='password' />
+        <Button go={'/'} size='m'>
+          로그인
+        </Button>
+      </InputForm>
+      <Link to='/login/signup' style={{ display: 'block', marginTop: '30px' }}>
+        회원가입
+      </Link>
+    </ContSection>
   );
 }
---
+++ src/pages/Login/LoginStyle.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+import { Cont, IR } from 'styles/GlobalStyles';
+
+const ContSection = styled.section`
+  ${Cont}
+  text-align: center;
+  color: var(--main-grey);
+`;
+
+const H2IR = styled.h2`
+  ${IR}
+`;
+
+const LogoImg = styled.img`
+  width: 238px;
+  height: 74px;
+  margin: 100px 0 70px 0;
+`;
+
+const InputForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 550px;
+  height: 320px;
+  margin: 0 auto;
+  text-align: left;
+  box-shadow: -1px 0 20px -1px #f2f2f2, 1px 0 20px -1px #f2f2f2;
+  border-radius: 10px;
+  padding: 35px;
+  button {
+    margin-top: 20px;
+  }
+  input {
+    width: 480px;
+    height: 60px;
+    border: transparent;
+    border-bottom: 1px solid #dbdbdb;
+    outline: none;
+    margin-bottom: 20px;
+    &:focus {
+      border-bottom: 2px solid var(--point);
+    }
+  }
+`;
+
+export { ContSection, H2IR, LogoImg, InputForm };
