afab71ceacd98db3ae1661856bdc79788828b244
konveloper <konveloper@gmail.com>
2023-04-16 22:10:14 +0900

    modify : Input 컴포넌트에 props 추가

--- src/components/Common/Input/Input.jsx
+++ src/components/Common/Input/Input.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { ContDiv, InputStyle, MessageSpan } from './InputStyle';
 
-function Input({ label, type, name, value, placeholder, message }) {
+function Input({
+  label,
+  type,
+  placeholder,
+  onChange,
+  message,
+  min,
+  max,
+  onBlur,
+  name,
+  getValue,
+  onKeyUp,
+}) {
+  const ChangeHandler = (e) => {
+    onChange(e);
+  };
   return (
     <ContDiv>
       <label htmlFor={type}>{label}</label>
@@ -9,8 +24,13 @@ function Input({ label, type, name, value, placeholder, message }) {
         name={name}
         type={type || 'text'}
         id={type}
-        value={value}
         placeholder={placeholder}
+        onBlur={onBlur}
+        onChange={ChangeHandler}
+        onKeyUp={onKeyUp}
+        minLength={min}
+        maxLength={max}
+        value={getValue}
       />
       <MessageSpan>{message}</MessageSpan>
     </ContDiv>
